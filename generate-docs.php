85bcb6ea34a21248a500a0fd42f244b5fe498f7b
Iusiel <iusiel@outlook.com>
2022-10-16 15:07:16 +0800

    Generated docs

--- generate-docs.php
+++ generate-docs.php
@@ -1,5 +1,4 @@
 <?php
-
 $generatedHTML = "";
 
 $globalCodeSnippets = file_get_contents("custom_snippets/global.code-snippets");
@@ -8,38 +7,53 @@ $globalCodeSnippets = json_decode($globalCodeSnippets);
 
 foreach ($globalCodeSnippets as $index => $snippet) {
     $generatedHTML .= sprintf('
-    <section>
+    <section style="border-bottom: 1px solid #000;padding-bottom: 30px;">
         <h2>%s</h2>
         <h3>%s</h3>
         <p>
         <mark><strong>%s</strong></mark> transforms to
         </p>
         <pre
+        onclick="copyCode(\'%s\')"
         title="Click to copy to clipboard"
         ><code class="language-html">%s</code></pre>
     </section>
-    ', $index, $snippet->description, $snippet->prefix, htmlentities(implode(PHP_EOL, $snippet->body)));
+    ', $index, $snippet->description, $snippet->prefix, base64_encode(implode(PHP_EOL, $snippet->body)), htmlentities(implode(PHP_EOL, $snippet->body)));
 }
-var_dump($generatedHTML);
+
+
+
+$it = new FilesystemIterator("custom_snippets");
+foreach ($it as $fileinfo) {
+    if ($fileinfo->getFileInfo()->getExtension() === 'json') {
+        $fileContents = file_get_contents($fileinfo->getPathname());
+        $fileContents = preg_replace("#(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|([\s\t]//.*)|(^//.*)#", '', $fileContents); // remove all comments
+        $fileContents = json_decode($fileContents);
+
+        foreach ($fileContents as $index => $snippet) {
+            $generatedHTML .= sprintf('
+            <section style="border-bottom: 1px solid #000;padding-bottom: 30px;">
+                <h2>%s</h2>
+                <h3>%s</h3>
+                <p>
+                <mark><strong>%s</strong></mark> transforms to
+                </p>
+                <pre
+                onclick="copyCode(\'%s\')"
+                title="Click to copy to clipboard"
+                ><code class="language-html">%s</code></pre>
+            </section>
+            ', $index, $snippet->description, $snippet->prefix, base64_encode(implode(PHP_EOL, $snippet->body)), htmlentities(implode(PHP_EOL, $snippet->body)));
+        }
+
+    } 
+}
+
+
+
+// echo PHP_EOL;
 
 $htmlInitialContents = file_get_contents('base.html');
 $replacedContents = str_replace('[slot]', $generatedHTML, $htmlInitialContents);
 
 file_put_contents("docs/index.html", $replacedContents);
-
-
-
-
-// $it = new FilesystemIterator("custom_snippets");
-// foreach ($it as $fileinfo) {
-//     if ($fileinfo->getFileInfo()->getExtension() === 'json') {
-//         $fileContents = file_get_contents($fileinfo->getPathname());
-//         $fileContents = preg_replace("#(/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/)|([\s\t]//.*)|(^//.*)#", '', $fileContents); // remove all comments
-//         $fileContents = json_decode($fileContents);
-//         var_dump($fileContents);
-//     }
-// }
-
-
-
-// echo PHP_EOL;
